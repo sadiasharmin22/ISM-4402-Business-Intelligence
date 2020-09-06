1c68af1a45643d49bbc7be87f2a0cf691775c0ce
sadiasharmin22 <70179474+sadiasharmin22@users.noreply.github.com>
2020-09-06 13:54:17 -0400

    Assignment 1 pg7

---
+++ Assignment 1pg.7.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+Location = "C:/Users/sadia/OneDrive/Desktop/Business intelligence/datasets/all_040_in_01.P1.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
