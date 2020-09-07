ffb182630b114c6fda8dbddfc11041476c732889
sadiasharmin22 <70179474+sadiasharmin22@users.noreply.github.com>
2020-09-07 08:27:27 -0400

    Assignment 1 pg9

---
+++ Assignment 1 pg9.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+Location = "C:/Users/sadia/OneDrive/Desktop/Business intelligence/datasets/Flag_Reference.xls"
+df = pd.read_excel(Location, header=None)
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
